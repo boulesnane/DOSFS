5c3a468b9cf016049f519c825120aac3a3f15c97
Abdennour Boulesnane (عبد النور بولسنان) <aboulesnane@univ-constantine3.dz>
2021-07-17 23:56:09 +0200

    Delete tools directory

--- tools/classlablesto1and0.m
+++
@@ -1,7 +0,0 @@
-for i=1:size(leukemia_std,1)
-if(leukemia_std(i,size(leukemia_std,2))==1)
-leukemia_std(i,size(leukemia_std,2))=1;
-elseif(leukemia_std(i,size(leukemia_std,2))==-1)
-leukemia_std(i,size(leukemia_std,2))=0;
-end
-end
